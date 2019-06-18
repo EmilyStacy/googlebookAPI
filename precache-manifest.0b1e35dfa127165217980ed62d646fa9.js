2d50cbb42e2353bcb358d98a0bca491078776547
Emily Stacy <beyonce@thinkful.com>
2019-06-18 07:40:03 -0500

    Updates

---
+++ precache-manifest.0b1e35dfa127165217980ed62d646fa9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "18bfd250910344acc913885d21528ef2",
+    "url": "/googlebookAPI/index.html"
+  },
+  {
+    "revision": "2e46ecf49b1b2c7e1afd",
+    "url": "/googlebookAPI/static/css/main.5dbdf4d0.chunk.css"
+  },
+  {
+    "revision": "baf961a1263ccd6a2d24",
+    "url": "/googlebookAPI/static/js/2.0f58faaa.chunk.js"
+  },
+  {
+    "revision": "2e46ecf49b1b2c7e1afd",
+    "url": "/googlebookAPI/static/js/main.693475ee.chunk.js"
+  },
+  {
+    "revision": "64dc51798fdf2b89fd7d",
+    "url": "/googlebookAPI/static/js/runtime~main.7655b8ba.js"
+  }
+]);
